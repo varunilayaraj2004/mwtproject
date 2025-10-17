9754b5b96f7675e6b242f0825d63349268cffe79
varunilayaraj2004 <varuncodeshadow2004@gmail.com>
2025-10-17 16:23:28 +0530

    addfile

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -114,11 +114,11 @@ function HeaderRightContent() {
             </AvatarFallback>
           </Avatar>
         </DropdownMenuTrigger>
-        <DropdownMenuContent side="right" className="w-56">
+        <DropdownMenuContent side="right" className="w-56 bg-white">
           <DropdownMenuLabel>Logged in as {user?.userName}</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem onClick={() => navigate("/shop/account")}>
-            <UserCog className="mr-2 h-4 w-4" />
+            <UserCog className="mr-2 h-4 w-4 bg-white" />
             Account
           </DropdownMenuItem>
           <DropdownMenuSeparator />
@@ -137,7 +137,7 @@ function ShoppingHeader() {
   // const { isAuthenticated } = useSelector((state) => state.auth);
 
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
+    <header className="sticky top-0 z-40 w-full border-b bg-blue-50">
       <div className="flex h-16 items-center justify-between px-4 md:px-6">
         <Link to="/shop/home" className="flex items-center gap-2">
           <HousePlug className="h-6 w-6" />
